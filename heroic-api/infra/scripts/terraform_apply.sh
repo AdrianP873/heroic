4d8c2c0bf1fc3870d8797df55e2beb6667d9d5a0
AdrianP873 <adrian.plummer@shippit.com>
2021-07-07 16:10:35 +1000

    Add tf apply script, and update pipeline

---
+++ heroic-api/infra/scripts/terraform_apply.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -x
+
+STATE_BUCKET="heroic-${REGION}-tf"
+
+terraform init \
+  -backend-config="bucket=${STATE_BUCKET}"
+  -backend-config="region=${REGION}"
+
+terraform apply -auto-approve
