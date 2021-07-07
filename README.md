ee661fed411a5395aef821c85c3af2ac40599d46
AdrianP873 <adrian.plummer@shippit.com>
2021-07-07 21:37:34 +1000

    Update app create

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # heroic
+
+## Requirements:
+- Your GitHub repo requires AWS credentials (AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY) configured as <a href="https://docs.github.com/en/actions/reference/encrypted-secrets">GitHub Actions secrets</a>. This allows the provisioned pipeline to push docker images to AWS ECR.
+
+## To do
+- Provision a bucket with Terraform to hold pipeline config files. Grant access to lambda function role.
--- heroic-api/src/app-create.py
+++ heroic-api/src/app-create.py
@@ -13,9 +13,37 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
     """ Build baseline pipeline """
-    # Store baseline pipeline config file in s3
-    # Lambda pulls down s3 file, reads it, and substitutes own values into file
-    # Lambda uses file
+    # Parse the app_name and github_uri passed to it
+    app_name = event["app_name"]
+    github_url = event["github_url"]
+
+    # Pull down config file from s3
+    s3 = boto3.client("s3")
+
+    data = s3.get_object(
+        Bucket="heroic-ap-southeast-2-pipeline-configs",
+        Key="python_pipeline.yml",
+    )
+    contents = data['Body'].read()
+    print(contents)
+    
+    with open("/tmp/test.txt", "w+") as f:
+        text = f.write(str(data['Body'].read()))
+
+    with open("/tmp/test.txt", "r+") as l:
+        text = l.read()
+        text = re.sub("NAME_PLACEHOLDER", "THIS_MA_NEW_NAME", text)
+        l.seek(0)
+        l.truncate()
+        print(text)
+
+    # Modify file to put in the app_name and github_uri
+    # Upload the file to the Github Repo
+
+
+
+
+
 
 # Whatever we pass can be accessed via event["key"]
 
