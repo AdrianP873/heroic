ced7bb023ecaa53ae1ef3a3b1f8b6282bc482dde
AdrianP873 <adrian.plummer@shippit.com>
2021-07-09 09:33:38 +1000

    Add error handling and build ECR function

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 ## To do
 - Provision a bucket with Terraform to hold pipeline config files. Grant access to lambda function role.
+- In app-create.py, think of a way to dynamically add the environment (and maybe owner) to ECR repository tags to keep consistency.
--- heroic-api/src/app-create.py
+++ heroic-api/src/app-create.py
@@ -3,6 +3,7 @@ Create an application.
 """
 
 import boto3
+from botocore.exceptions import ClientError
 import logging
 import re, json
 
@@ -11,9 +12,17 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     """ Build baseline pipeline """
-    # Parse the app_name and github_uri passed to it
+    # Parse the app_name and github_url passed to it
     app_name = event["app_name"]
+    print(app_name)
     github_url = event["github_url"]
+
+    search_repo = re.search("/(.+?).git", github_url)
+    if search_repo:
+        repo_name = search_repo.group(1)
+        logging.info("Repository name {} found.".format(repo_name))
+    else:
+        logging.warning("Couldn't find repository name from git URL.")
 
     # Pull down config file from s3
     logging.info("Pulling down config file python_pipeline.yml")
@@ -22,11 +31,11 @@ def lambda_handler(event, context):
 
     data = s3.get_object(
         Bucket="heroic-ap-southeast-2-pipeline-configs",
-        Key="python_pipeline.yml",
+        Key="python_pipeline.yml"
     )
 
     # Modify file to put in the app_name and github_uri
-    logging.info("Modifying config pipeline file with application name: {}".format(app_name)
+    logging.info("Modifying config pipeline file with application name: {}".format(app_name))
 
     contents = data['Body'].read()
     
@@ -40,13 +49,47 @@ def lambda_handler(event, context):
         print(text)
 
     # Upload the file to the Github Repo
+    ecr = boto3.client("ecr")
 
-def build_ecr_repo():
-    # Check if ECR Repo under app_name exists
-    # If repo does not already exist, create one with appropriate repository policy
-    pass
+    try:
+        response = ecr.describe_repositories(
+            repositoryNames=[
+                app_name
+            ],
+        )
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "RepositoryNotFoundException":
+            print("Repository {} not found, creating one instead...".format(app_name))
+            logging.info("Repository {} not found, creating one instead...".format(app_name))
+
+            create_repo = ecr.create_repository(
+                repositoryName=app_name,
+                tags=[
+                    {
+                        "repo": repo_name,
+                        "env": dev
+                    }
+                ],
+                imageScanningConfiguration={
+                    "scanOnPush": True
+                },
+                encryptionConfiguration={
+                    encryptionType: "KMS"
+                }
+            )
+        else:
+            print("Something went wrong...")
+            print(e.response)
+            logging.error("Something went wrong...")
 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+def build_ecr_repo():
+    """ Creates new ECR repo if one doesn't already exist """
+    # Check if ECR Repo under app_name exists. If not, create new one
+    # Need to pass app_name to this function 
+    pass
+
