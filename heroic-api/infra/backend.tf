13735972340abd17e6028076b2f66ebfdc50ecb8
AdrianP873 <adrian.m.plummer@gmail.com>
2021-07-16 12:35:42 +1000

    Fix tf state bucket configuration

--- heroic-api/infra/backend.tf
+++ heroic-api/infra/backend.tf
@@ -1,5 +1,7 @@
 terraform {
-    backend "remote" {
-        key = "heroic-api/terraform.tfstate"
+    backend "s3" {
+        bucket = "heroic-ap-southeast-2-tf"
+        key = "terraform.tfstate"
+        region = "ap-southeast-2"
     }
 }
