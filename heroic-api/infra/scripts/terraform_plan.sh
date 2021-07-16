32eda9dfb07ce0d46bdbce33d1fa254a65f8db40
AdrianP873 <adrian.m.plummer@gmail.com>
2021-07-16 12:25:15 +1000

    update ci

---
+++ heroic-api/infra/scripts/terraform_plan.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -x
+
+STATE_BUCKET="heroic-${REGION}-tf"
+
+terraform init \
+  -backend-config="bucket=${STATE_BUCKET}"
+  -backend-config="region=${REGION}"
+
+terraform workspace list
+terraform workspace select ${ENV} || terraform workspace new ${ENV}
+
+terraform plan
+
+terraform show
