f8cfe42e2adf7b21a535ce466e376fc5c8ae8339
AdrianP873 <adrian.m.plummer@gmail.com>
2021-07-23 07:53:32 +1000

    Add chart test script, make chart_template.sh more dynamic, changed chart workflow to app chart workflow

--- heroic-api/scripts/chart_template.sh
+++ heroic-api/scripts/chart_template.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
+# Lints helm chart and renders chart templates locally and displaying the output.
+
+set -e
+
+CHART=$1
+
 echo "Linting helm chart..."
-helm lint charts/application
+helm lint charts/${CHART}
 
 echo "Templating helm chart..."
 helm template \
-    test-chart \
-    charts/application \
+    test-${CHART}-chart \
+    charts/${CHART} \
   --namespace default \
   --atomic \
-  -f charts/tests/application-values.yml \
+  -f charts/tests/${CHART}-values.yml \
   --output-dir charts/tests/test-outputs
---
+++ heroic-api/scripts/chart_test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Installs the test pods and tests the helm charts.
+
+set -e
+
+CHART=$1
+
+echo "Installing helm chart: ${CHART}"
+
+helm install \
+    ${CHART}-test ${CHART} \
+    --namespace default \
+    --atomic \
+    -f charts/tests/${CHART}-values.yml
+
+echo "Testing helm chart: ${CHART}"
+
+helm test \
+    ${CHART}-test \
+    --namespace default
+
+helm uninstall \
+    ${CHART}-test \
+    --namespace default
